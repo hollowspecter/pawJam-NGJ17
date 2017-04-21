84352bfc1d787e3c07f2ee1bae1385777c28f4c4
Vivien <hollowspecder@googlemail.com>
2017-04-22 00:22:43 +0200

    SimpleMouseController

--- Assets/Scripts/Mouse/MouseController.cs
+++ Assets/Scripts/Mouse/MouseController.cs
@@ -12,8 +12,13 @@ public class MouseController : MonoBehaviour {
     private float m_fSpeed;
     [SerializeField]
     private float m_fAxisThreshold;
+    [Range(0f, 1f)]
     [SerializeField]
-    //private float m_f
+    private float m_fMinimumSpeedfactor = 0.5f;
+    [SerializeField]
+    private bool m_bMoveByForce = false;
+    [SerializeField]
+    private float m_fForceMultiplier = 10f;
 
     private Rigidbody m_rigid;
     private Transform m_transform;
@@ -24,17 +29,39 @@ public class MouseController : MonoBehaviour {
         m_transform = GetComponent<Transform>();
     }
 	
-	void Update () {
+	void FixedUpdate () {
         // fetch movement from axis
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        
-        // make it snappy
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
+        bool walking = h != 0f || v != 0f;
+        Move(h, v);
+        Turning(h, v, walking);
 
-        Vector3 newPosition = m_transform.position + movement * Time.deltaTime * m_fSpeed;
-        m_rigid.MovePosition(newPosition);
-
+        // debug
         if (m_debugText != null)
-            m_debugText.text = "X: " + x + "; Y: " + y;
+            m_debugText.text = "X: " + h + "; Z: " + v;
+    }
+
+    void Move(float h, float v)
+    {
+        Vector3 movement = new Vector3(h, 0f, v);
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * m_fSpeed * Time.deltaTime;
+
+        if (!m_bMoveByForce)
+            m_rigid.MovePosition(transform.position + movement); //current pos + movement
+        else
+            m_rigid.AddForce(movement * m_fForceMultiplier);
+    }
+
+    void Turning(float h, float v, bool isWalking)
+    {
+        if (isWalking)
+        {
+            Vector3 movement = new Vector3(h, 0.0f, v);
+            Quaternion newRotation = Quaternion.LookRotation(movement);
+            m_rigid.MoveRotation(newRotation);
+        }
     }
 }
