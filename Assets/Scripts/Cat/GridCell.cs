54ddd3ce5a98f60fd7c9a58e4a740c4356d66ce2
Shentoza <wuelk@hotmail.de>
2017-04-22 03:35:43 +0200

    Prefabs

--- Assets/Scripts/Cat/GridCell.cs
+++ Assets/Scripts/Cat/GridCell.cs
@@ -11,21 +11,29 @@ public class GridCell : MonoBehaviour {
     private MeshRenderer m_meshR;
 
     public string triggerKey { get; set; }
+    public GameObject paw;
+    float pawHeight;
 
 	// Use this for initialization
 	void Start () {
         m_meshR = this.GetComponent<MeshRenderer>();
         m_meshR.material.color = basic;
+        paw = GameObject.FindGameObjectWithTag("Paw");
+        pawHeight = paw.transform.position.y;
 	}  
 	
 	// Update is called once per frame
 	void Update () {
         if(!string.IsNullOrEmpty(triggerKey)) {
             if (Input.GetKeyDown(triggerKey)) {
+                paw.transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
                 m_meshR.material.color = lighted;
+
             }
             if(Input.GetKeyUp(triggerKey)) {
+                paw.transform.position = new Vector3(transform.position.x, pawHeight, transform.position.z);
                 m_meshR.material.color = basic;
+
             }
         }   
 	}
