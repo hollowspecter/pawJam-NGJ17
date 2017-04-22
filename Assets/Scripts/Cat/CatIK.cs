fca3360d03b0d641dc6ae4068bd01c497ca77d35
Vivien <hollowspecder@googlemail.com>
2017-04-22 20:29:48 +0200

    added paw

---
+++ Assets/Scripts/Cat/CatIK.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatIK : MonoBehaviour {
+
+    [SerializeField]
+    private bool ikActive = false;
+    [SerializeField]
+    private Transform head = null;
+    [SerializeField]
+    private Transform lookObj = null;
+
+    private Animator m_anim;
+
+    void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+    }
+}
