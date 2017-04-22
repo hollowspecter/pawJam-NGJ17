dbf2658102634b0a51f2560a41a4131c3edc9cdb
Vivien <hollowspecder@googlemail.com>
2017-04-22 04:50:08 +0200

    programmed a sweet turrdeturr

---
+++ Assets/Scripts/Utility/FixedSizeQueue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedSizeQueue<T> : Queue<T>
+{
+
+    public int Limit { get; set; }
+
+    public FixedSizeQueue(int limit) : base(limit)
+    {
+        Limit = limit;
+    }
+
+    public new void Enqueue(T item)
+    {
+        while (Count >= Limit)
+        {
+            Dequeue();
+        }
+        base.Enqueue(item);
+    }
+}
