b105ec6304eddfcc62fe0b360f566263245acaf9
Vivien <hollowspecder@googlemail.com>
2017-04-22 15:10:26 +0200

    Added Pickup Spawner on Battlefield

---
+++ Assets/Scripts/Other/PickUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+	void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Mouse"))
+        {
+            Activate();
+            Destroy(gameObject);
+        }
+    }
+
+    void Activate()
+    {
+        // do the action here
+        print("pickup");
+    }
+}
---
+++ Assets/Scripts/Other/PickUpSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(GridCreator))]
+public class PickUpSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private Vector2 m_minMaxSpawnCooldown = new Vector2(20f, 50f);
+    [SerializeField]
+    private GameObject m_prefabPickUp;
+
+    private float timer = 0f;
+    private GridCreator gridder;
+    private int lastGridIndex;
+
+    void Awake()
+    {
+        Assert.IsNotNull<GameObject>(m_prefabPickUp);
+        gridder = GetComponent<GridCreator>();
+        Assert.IsNotNull<GridCreator>(gridder);
+    }
+
+	void Update () {
+        if (timer > 0f)
+            timer -= Time.deltaTime;
+        else
+        {
+            // spawn one randomly
+            // calc location
+            int index = Random.Range(0, gridder.CellList.Count);
+            index += (index == lastGridIndex) ? 1 : 0;
+            lastGridIndex = index;
+            Transform randomCell = ((GameObject) gridder.CellList[index]).transform;
+
+            GameObject pickup = Instantiate<GameObject>(m_prefabPickUp);
+            pickup.transform.position = randomCell.position;
+             
+            // set a new timer for the next cooldown
+            timer = Random.Range(m_minMaxSpawnCooldown.x, m_minMaxSpawnCooldown.y);
+            print("Spawn Pickup, next timer on " + timer);
+        }
+	}
+}
